be663185aa0616a40e34b88564365df59b555443
Cremacious <chrismackall3@gmail.com>
2025-04-02 18:59:21 -0400

    Created new quick create component

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -1,6 +1,5 @@
 'use client';
-import { useSidebar } from '@/components/ui/sidebar';
-import { BookUser, CalendarFold, LayoutDashboard, Plus } from 'lucide-react';
+
 import {
   SidebarGroup,
   SidebarGroupContent,
@@ -9,7 +8,10 @@ import {
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
 
+import { LayoutDashboard } from 'lucide-react';
 import Link from 'next/link';
+import QuickCreateForm from './quick-create-form';
+import { useSidebar } from '@/components/ui/sidebar';
 
 export function NavMain() {
   const { toggleSidebar } = useSidebar();
@@ -18,41 +20,77 @@ export function NavMain() {
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              onClick={toggleSidebar}
-              tooltip="Quick Create"
-              className="bg-cyan-500 text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
-            >
-              <Plus />
-              <span>Quick Create</span>
+            <SidebarMenuButton className="bg-cyan-500 justify-center text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear">
+              <QuickCreateForm />
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
         <SidebarMenu>
-          <SidebarMenuItem>
-            <Link href="/dashboard">
-              <SidebarMenuButton onClick={toggleSidebar} tooltip="">
-                <LayoutDashboard className="text-sidebar-foreground/70" />
-                <span>Dashboard</span>
-              </SidebarMenuButton>
-            </Link>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <Link href="/dashboard/events">
-              <SidebarMenuButton onClick={toggleSidebar} tooltip="">
-                <CalendarFold className="text-sidebar-foreground/70" />
-                <span>My Events</span>
-              </SidebarMenuButton>
-            </Link>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <Link href="/dashboard/friends">
-              <SidebarMenuButton onClick={toggleSidebar} tooltip="">
-                <BookUser className="text-sidebar-foreground/70 " />
-                <span>Friends</span>
-              </SidebarMenuButton>
-            </Link>
-          </SidebarMenuItem>
+          <div className="md:hidden">
+            <SidebarMenuItem>
+              <Link href="/dashboard">
+                <SidebarMenuButton onClick={toggleSidebar} tooltip="">
+                  <LayoutDashboard className="text-sidebar-foreground/70" />
+                  <span>Dashboard</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          </div>
+
+          <div className="hidden md:block">
+            <SidebarMenuItem>
+              <Link href="/dashboard">
+                <SidebarMenuButton tooltip="">
+                  <LayoutDashboard className="text-sidebar-foreground/70" />
+                  <span>Dashboard</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          </div>
+
+          <div className="md:hidden">
+            <SidebarMenuItem>
+              <Link href="/dashboard/events">
+                <SidebarMenuButton onClick={toggleSidebar} tooltip="">
+                  <LayoutDashboard className="text-sidebar-foreground/70" />
+                  <span>My Events</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          </div>
+
+          <div className="hidden md:block">
+            <SidebarMenuItem>
+              <Link href="/dashboard/events">
+                <SidebarMenuButton tooltip="">
+                  <LayoutDashboard className="text-sidebar-foreground/70" />
+                  <span>My Events</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          </div>
+
+          <div className="md:hidden">
+            <SidebarMenuItem>
+              <Link href="/dashboard/friends">
+                <SidebarMenuButton onClick={toggleSidebar} tooltip="">
+                  <LayoutDashboard className="text-sidebar-foreground/70" />
+                  <span>Friends</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          </div>
+
+          <div className="hidden md:block">
+            <SidebarMenuItem>
+              <Link href="/dashboard/friends">
+                <SidebarMenuButton tooltip="">
+                  <LayoutDashboard className="text-sidebar-foreground/70" />
+                  <span>Friends</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          </div>
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
