5d3dd25a84d8a8a552eb018eed387b4b579f5631
Cremacious <chrismackall3@gmail.com>
2025-04-03 10:51:04 -0400

    Integrated Prisma

---
+++ db/seed.ts
@@ -0,0 +1,16 @@
+// import { PrismaClient } from '@prisma/client';
+// import sampleData from './sample-data';
+
+// async function main() {
+//   const prisma = new PrismaClient();
+//   await prisma.event.deleteMany();
+
+//   await prisma.event.createMany({ data: sampleData.event });
+
+//   console.log('Database seeded successfully');
+// }
+
+// main();
+
+
+
