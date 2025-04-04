4b6294c539a56336fb53ef484dd760584eb43b03
Cremacious <chrismackall3@gmail.com>
2025-04-04 17:30:34 -0400

    Event actions can now create events

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,41 +1,32 @@
-// import { currentUser } from '@clerk/nextjs/server';
-// import { db } from '@/lib/db';
-// import { insertEventSchema } from '../validators';
-// import { z } from 'zod';
+import { auth } from '@/auth';
+import { insertEventSchema } from '../validators';
+import { prisma } from '@/db/prisma';
+import { z } from 'zod';
 
-// export async function createEvent(data: z.infer<typeof insertEventSchema>) {
-//   try {
-//     const event = insertEventSchema.parse(data);
+export async function createEvent(data: z.infer<typeof insertEventSchema>) {
+  try {
+    const session = await auth();
+    if (!session) {
+      throw new Error('User not authenticated');
+    }
+    const userId = session?.user?.id;
 
-//     const user = await currentUser();
-
-//     if (!user) {
-//       throw new Error('User is not authenticated.');
-//     }
-
-//     // Find the user in the database by Clerk user ID
-//     const dbUser = await db.user.findUnique({
-//       where: {
-//         clerkUserId: user.id,
-//       },
-//     });
-
-//     if (!dbUser) {
-//       throw new Error('User not found in the database.');
-//     }
-
-//     await db.event.create({
-//       data: {
-//         ...event,
-//         user: {
-//           connect: {
-//             id: dbUser.id,
-//           },
-//         },
-//       },
-//     });
-//     return { success: true, message: 'Event created successfully' };
-//   } catch (error) {
-//     return { success: false, message: error };
-//   }
-// }
+    const dbUser = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!dbUser) {
+      throw new Error('User not found in the database.');
+    }
+    const event = insertEventSchema.parse(data);
+    await prisma.event.create({
+      data: {
+        ...event,
+        userId: dbUser.id,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
