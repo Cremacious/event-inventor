48fc3a9a47f398af62f3b428256988e0849fa819
Cremacious <chrismackall3@gmail.com>
2025-04-04 10:24:07 -0400

    Can successfully capture form data

--- components/shared/events/create-event-form.tsx
+++ components/shared/events/create-event-form.tsx
@@ -1,11 +1,37 @@
+'use client';
 
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { insertEventSchema } from '@/lib/validators';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const CreateEventForm = () => {
+  const form = useForm<z.infer<typeof insertEventSchema>>({
+    resolver: zodResolver(insertEventSchema),
+  });
 
-  return <>
-    
-  
-  </>;
+  const onSubmit: SubmitHandler<z.infer<typeof insertEventSchema>> = (data) => {
+    console.log('Form submitted');
+    console.log(data);
+  };
+
+  return (
+    <>
+      <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+        <Input
+          placeholder="Event Name"
+          {...form.register('name')}
+          className="w-full"
+        />
+        <Button variant="destructive" type="submit">
+          Create Event
+        </Button>
+      </form>
+    </>
+  );
 };
 
 export default CreateEventForm;
--- lib/validators.ts
+++ lib/validators.ts
@@ -1,9 +1,13 @@
 import { z } from 'zod';
 
+// export const insertEventSchema = z.object({
+//   name: z.string().min(1, { message: 'Name is required' }),
+//   description: z.string().min(1, { message: 'Description is required' }),
+//   type: z.string().min(1, { message: 'Type is required' }),
+//   date: z.string().min(1, { message: 'Date is required' }),
+//   visibility: z.string().min(1, { message: 'Visibility is required' }),
+// });
+
 export const insertEventSchema = z.object({
-  name: z.string().min(1, { message: 'Name is required' }),
-  description: z.string().min(1, { message: 'Description is required' }),
-  type: z.string().min(1, { message: 'Type is required' }),
-  date: z.string().min(1, { message: 'Date is required' }),
-  visibility: z.string().min(1, { message: 'Visibility is required' }),
+  name: z.string().min(1, 'Event name is required'),
 });
