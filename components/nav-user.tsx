b120709c545648b14cade698812f910ed376c47a
Cremacious <chrismackall3@gmail.com>
2025-04-04 21:50:44 -0400

    User button now works

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -2,20 +2,9 @@ import { SidebarMenu, SidebarMenuItem } from '@/components/ui/sidebar';
 
 import Link from 'next/link';
 import { Settings } from 'lucide-react';
-
-// import { auth, signOut } from '@/auth';
-
-// import { Button } from './ui/button';
+import UserButton from './user-button';
 
 export async function NavUser() {
-  // const session = await auth();
-
-  // const user = session?.user;
-
-  // const handleSignOut = async () => {
-  //   await signOut();
-  // };
-
   return (
     <SidebarMenu
       className="backgroundCyan
@@ -23,9 +12,7 @@ export async function NavUser() {
     >
       <SidebarMenuItem>
         <div className=" flex flex-row">
-          {/* <Button variant="ghost" className="w-full" onClick={handleSignOut}>
-            Sign Out
-          </Button> */}
+          <UserButton />
           <div className="ml-2 grid flex-1 text-left text-sm text-slate-800">
             <span className="truncate font-medium">name</span>
             <span className="text-slate-800 truncate text-xs">email</span>
