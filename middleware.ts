4c33ba244ea2993e62f6ee7a3140830c3cace0ac
Cremacious <chrismackall3@gmail.com>
2025-04-05 16:04:29 -0400

    Updated middleware for routing when not signed in

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,13 @@ import { getToken } from 'next-auth/jwt';
 export async function middleware(req: Request) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
+  const { pathname } = new URL(req.url);
+
+  // Exclude the /sign-in route from middleware checks
+  if (pathname.startsWith('/sign-in')) {
+    return NextResponse.next();
+  }
+
   // If no token is found, redirect to the sign-in page
   if (!token) {
     const url = new URL('/sign-in', req.url);
