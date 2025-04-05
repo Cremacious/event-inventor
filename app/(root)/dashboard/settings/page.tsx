af923fb675b547f42e22437d4f94b3fc76db7795
Cremacious <chrismackall3@gmail.com>
2025-04-05 08:54:09 -0400

    Removed unused Clerk import

--- app/(root)/dashboard/settings/page.tsx
+++ app/(root)/dashboard/settings/page.tsx
@@ -1,10 +1,10 @@
-import { UserProfile } from '@clerk/nextjs';
+
 
 const SettingsPage = () => {
   return (
     <>
       <div className="container flex justify-center items-center h-full">
-        <UserProfile />
+ 
       </div>
     </>
   );
