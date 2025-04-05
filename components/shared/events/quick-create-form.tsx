688a5819c355eb122af801390314065ff7f93e68
Cremacious <chrismackall3@gmail.com>
2025-04-05 15:06:37 -0400

    Dialog now closes on submit

--- components/shared/events/quick-create-form.tsx
+++ components/shared/events/quick-create-form.tsx
@@ -17,10 +17,12 @@ import { createEvent } from '@/lib/actions/event.actions';
 import flaskImage from '@/public/images/icons/flask-bubble.png';
 import { insertEventSchema } from '@/lib/validators';
 import { toast } from 'sonner';
+import { useState } from 'react';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const QuickCreateForm = () => {
+  const [open, setOpen] = useState(false);
   const form = useForm<z.infer<typeof insertEventSchema>>({
     resolver: zodResolver(insertEventSchema),
   });
@@ -30,6 +32,7 @@ const QuickCreateForm = () => {
   ) => {
     const response = await createEvent(data);
     if (response.success) {
+      setOpen(false);
       toast.success(response.message);
     } else {
       toast.error(response.message);
@@ -37,7 +40,7 @@ const QuickCreateForm = () => {
   };
 
   return (
-    <AlertDialog>
+    <AlertDialog open={open} onOpenChange={setOpen}>
       <AlertDialogTrigger asChild>
         <Button className="backgroundCyan w-full justify-center text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear">
           Quick Create Event
